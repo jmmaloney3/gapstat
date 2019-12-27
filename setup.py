30c2358d175c9b8d1e463aa987f1a73ec8b10b3f
John Maloney <jmmaloney3@gmail.com>
2019-12-26 23:48:42 -0600

    added required packags to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name="gapstat",
       version = '0.0.1',
       author = 'John M. Maloney',
       package_dir = {'':'src'},
-      py_modules = ['gapstat']
+      py_modules = ['gapstat'],
+      install_requires = ['scikit-learn']
 )
