42fb36f4abf7ddcac35193ac476c3c8997f1b58b
John Maloney <jmmaloney3@gmail.com>
2019-12-26 21:36:20 -0600

    enable module installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="gapstat",
+      version = '0.0.1',
+      author = 'John M. Maloney',
+      package_dir = {'':'src'},
+      py_modules = ['gapstat']
+)
